a026ef9b0870ae7c1432c69979ab92f21afddb06
Jerad Rutnam <jeradrutnam@outlook.com>
2016-08-19 08:27:05 +0530

    Updated the code with CodeMirror Multiplex Mode

--- main.js
+++ main.js
@@ -1,39 +1,58 @@
-define(function (require, exports, module) {
-    
-    var CodeMirror = brackets.getModule("thirdparty/CodeMirror/lib/codemirror"),
-        LanguageManager = brackets.getModule("language/LanguageManager");
-    
-    CodeMirror.defineSimpleMode("jaggery", {
-	    start: [
-	      	{
-	      		regex: /<!DOCTYPE[^>[]*(\[[^]]*\])?>/,
-	      		token: "comment"
-	      	},
-	      	{
-	      		regex: /<%.?/,
-	      		mode: {
-	      			spec: 'javascript',
-	      			end: /%>/
-	      		},
-	      		token: "tag"
-	      	},
-			{
-	      		regex: /^|<(?!%|!)/,
-	      		mode: {
-	      			spec: 'htmlmixed',
-	      			end: /(?:[^%])(>)/
-	      		},
-	      		token: 'tag' 
-	      	}
-	    ]
-  	});
+/*
+ * Copyright (c) 2016 Jerad Rutnam (http://jeradrutnam.com). All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
 
-    CodeMirror.defineMIME("text/x-jaggery", "jaggery");
+define(function (require, exports, module) {
+    "use strict";
+
+    var LanguageManager = brackets.getModule("language/LanguageManager"),
+        CodeMirror      = brackets.getModule("thirdparty/CodeMirror/lib/codemirror");
+
+    brackets.getModule(["thirdparty/CodeMirror/mode/handlebars/handlebars", "thirdparty/CodeMirror/mode/javascript/javascript"], function () {
+        
+        CodeMirror.defineMode("jaggery", function (config) {
+        	return CodeMirror.multiplexingMode(
+                CodeMirror.getMode(config, "text/html"),
+                {
+                    open: "<%",
+                    close: "%>",
+                    mode: CodeMirror.getMode(config, "javascript"),
+                    parseDelimiters: true
+                },
+                {
+                    open: "{{",
+                    close: "}}",
+                    mode: CodeMirror.getMode(config, "handlebars"),
+                    parseDelimiters: true
+                }
+            );
+        });
+        
+        CodeMirror.defineMIME("text/x-jaggery", "jaggery");
     
-    LanguageManager.defineLanguage("jaggery", {
-        name: "Jaggery",
-        mode: ["jaggery", "text/x-jaggery"],
-        fileExtensions: ["jag"]
+	    LanguageManager.defineLanguage("jaggery", {
+	        name: "Jaggery",
+	        mode: ["jaggery", "text/x-jaggery"],
+	        fileExtensions: ["jag"]
+	    });
     });
-    
 });
